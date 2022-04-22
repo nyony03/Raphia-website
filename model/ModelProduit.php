e1f77eb9013d60616ae23af280f52674838744fe
Obada TABBAB <aubadat3@gmail.com>
2022-04-22 14:32:42 +0200

    moodelProduit le constructeur cree

--- model/ModelProduit.php
+++ model/ModelProduit.php
@@ -1,19 +1,21 @@
 <?php
-class ModelProduit {
+class ModelProduit
+{
     private $idPoduit;
     private $nomProduit;
     private $prixProduit;
     private $cathegorieProduit;
 
     public function __construct($idProduit = NULL, $nomProduit = NULL,
-                                $prixProduit = NULL, $cathegorieProduit = NULL){
+                                $prixProduit = NULL, $cathegorieProduit = NULL)
+    {
 
-        if (!is_null($idProduit)&& !is_null($nomProduit)&&
-            !is_null($prixProduit)&&!is_null($cathegorieProduit)){
-            $this -> $idProduit = $idProduit;
-            $this -> $nomProduit = $nomProduit;
-            $this -> $prixProduit = $prixProduit;
-            $this -> $cathegorieProduit = $cathegorieProduit;
+        if (!is_null($idProduit) && !is_null($nomProduit) &&
+            !is_null($prixProduit) && !is_null($cathegorieProduit)) {
+            $this->$idProduit = $idProduit;
+            $this->$nomProduit = $nomProduit;
+            $this->$prixProduit = $prixProduit;
+            $this->$cathegorieProduit = $cathegorieProduit;
         }
 
 
