431e516c4311f0e6d19450ba6a6b9df9d3d3cabe
Ny Ony Rakotondramonja <ny-ony.rakotondramonja01@etu.umontpellier.fr>
2022-04-22 16:14:27 +0200

    controller

--- controller/ControllerProduit.php
+++ controller/ControllerProduit.php
@@ -1,6 +1,9 @@
 <?php
-
+require_once File::build_path(array("model","ModelProduit.php")); // chargement du modèle
 class ControllerProduit
 {
-
+    public static function readAll() {
+        $tab_v = ModelProduit::getAllProduit();     //appel au modèle pour gerer la BD
+        require ('view/produit/accueil.php');  //"redirige" vers la vue
+    }
 }
