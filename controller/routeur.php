738418964014f665e9af01aaa9f273ee521390f4
Obada TABBAB <aubadat3@gmail.com>
2022-04-22 16:52:33 +0200

    PLus de message d'erreur dans index

--- controller/routeur.php
+++ controller/routeur.php
@@ -1,7 +1,14 @@
 <?php
 
-require_once File::build_path(array("produit","accueil.php"));
+require_once File::build_path(array("./view/produit","accueil.php"));
 // On recupère l'action passée dans l'URL
 $action = $_GET["action"];
+
+if (!isset($action)){
+    echo "Bienvenue sur mon super site de nap";
+
+}
+else {
 // Appel de la méthode statique $action de ControllerVoiture
-ControllerProduit::$action();
+    ControllerProduit::$action();
+}
