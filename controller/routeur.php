1d3f8bb06a2fd56fe6a35b6a3843fb5681eb5f3c
Ny Ony Rakotondramonja <ny-ony.rakotondramonja01@etu.umontpellier.fr>
2022-04-22 14:15:41 +0200

    mvc

--- controller/routeur.php
+++ controller/routeur.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once File::build_path(array("controller","ControllerVoiture.php"));
+require_once File::build_path(array("produit","accueil.php"));
 // On recupère l'action passée dans l'URL
 $action = $_GET["action"];
 // Appel de la méthode statique $action de ControllerVoiture
-ControllerVoiture::$action();
+ControllerProduit::$action();
