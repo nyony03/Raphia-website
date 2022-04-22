d4adabe796ce356a1ed090e29f0952e68270b91e
Obada TABBAB <aubadat3@gmail.com>
2022-04-22 14:30:00 +0200

    Constructeur modele produit creer

--- model/ModelProduit.php
+++ model/ModelProduit.php
@@ -1,1 +1,21 @@
 <?php
+class ModelProduit {
+    private $idPoduit;
+    private $nomProduit;
+    private $prixProduit;
+    private $cathegorieProduit;
+
+    public function __construct($idProduit = NULL, $nomProduit = NULL,
+                                $prixProduit = NULL, $cathegorieProduit = NULL){
+
+        if (!is_null($idProduit)&& !is_null($nomProduit)&&
+            !is_null($prixProduit)&&!is_null($cathegorieProduit)){
+            $this -> $idProduit = $idProduit;
+            $this -> $nomProduit = $nomProduit;
+            $this -> $prixProduit = $prixProduit;
+            $this -> $cathegorieProduit = $cathegorieProduit;
+        }
+
+
+    }
+}
