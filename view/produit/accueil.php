5fe413db42a2d68d099d031139852ad1271eccba
Ny Ony Rakotondramonja <ny-ony.rakotondramonja01@etu.umontpellier.fr>
2022-05-04 13:25:15 +0200

    accueil pas fini

--- view/produit/accueil.php
+++ view/produit/accueil.php
@@ -1,9 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Raphia</title>
+    <title>raphia</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abhaya+Libre&amp;display=swap">
     <link rel="stylesheet" href="../assets/css/Articles-Cards.css">
@@ -12,74 +13,74 @@
 </head>
 
 <body>
-<nav class="navbar navbar-light navbar-expand-md" style="background: #ebd9d5;">
-    <div class="container-fluid">
-        <img src="../assets/img/logo.png" width="50" height="50"><a class="navbar-brand" href="#" style="color: #a75b5b;"><strong><em>Raphia</em></strong></a>
-        <div class="collapse navbar-collapse" id="navcol-1">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link active d-md-flex justify-content-md-start" href="#"></a></li>
-                <li class="nav-item"></li>
-                <li class="nav-item"></li>
-            </ul>
-        </div><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;margin: 10px;">Connexion</button><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;">Panier</button>
-    </div>
-</nav>
-<div id="promo" style="background: url('../assets/img/background.jpeg') left / cover no-repeat;";">
-    <div class="container" style="background: rgba(235,217,213,0.78);">
-        <h1 style="font-family: 'Abhaya Libre', serif;">Bienvenue sur Raphia</h1>
-        <p style="font-style: italic;font-family: 'Abhaya Libre', serif;font-size: 20px;">Découvrez l'art de toute une île</p>
-        <nav class="navbar navbar-light navbar-expand-md">
-            <div class="container-fluid"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navcol-2">
-                    <ul class="navbar-nav mx-auto">
-                        <li class="nav-item"><a class="nav-link active" href="#" style="color: #a75b5b;font-weight: bold;font-size: 20px;">Décoration</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#" style="color: #a75b5b;font-weight: bold;font-size: 20px;">Plage</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#" style="color: #a75b5b;font-weight: bold;font-size: 20px;">Mode</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-</div>
-<div id="article">
-    <div class="container py-4 py-xl-5">
-        <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3">
-            <div class="col">
-                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/drapPlage.png">
-                    <div class="card-body p-4">
-                        <h4 class="card-title">Title</h4>
-                        <p class="card-text">Description</p>
-                        <p class="card-text">Prix</p><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;">AJOUT AU PANIER</button>
+    <nav class="navbar navbar-light navbar-expand-md" style="background: #ebd9d5;">
+        <div class="container-fluid">
+            <picture><img src="../assets/img/logo.png" width="50" height="50"></picture><a class="navbar-brand" href="#" style="color: #a75b5b;"><strong><em>Raphia</em></strong></a>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active d-md-flex justify-content-md-start" href="#"></a></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                </ul>
+            </div><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;margin: 10px;">Connexion</button><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;">Panier</button>
+        </div>
+    </nav>
+    <div id="promo" style="background: url('../assets/img/photo-accueil.png') left / cover no-repeat;">
+        <div class="container" style="background: rgba(235,217,213,0.78);">
+            <h1 style="font-family: 'Abhaya Libre', serif;">Bienvenue sur Raphia</h1>
+            <p style="font-style: italic;font-family: 'Abhaya Libre', serif;font-size: 20px;">Découvrez l'art de toute une île</p>
+            <nav class="navbar navbar-light navbar-expand-md">
+                <div class="container-fluid"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navcol-2">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item"><a class="nav-link active" href="#" style="color: #a75b5b;font-weight: bold;">Décoration</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#" style="color: #a75b5b;font-weight: bold;">Plage</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#" style="color: #a75b5b;font-weight: bold;">Mode</a></li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/chapeau.png">
-                    <div class="card-body p-4">
-                        <h4 class="card-title">Title</h4>
-                        <p class="card-text">Description</p>
-                        <p class="card-text">Prix</p><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;">AJOUT AU PANIER</button>
+            </nav>
+        </div>
+    </div>
+    <div id="article">
+        <div class="container py-4 py-xl-5">
+            <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3">
+                <div class="col">
+                    <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/drapPlage.png">
+                        <div class="card-body p-4">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Description</p>
+                            <p class="card-text">Prix</p><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;">AJOUT AU PANIER</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card"><img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/chapeau.png">
+                        <div class="card-body p-4">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Description</p>
+                            <p class="card-text">Prix</p><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;">AJOUT AU PANIER</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div>
-    <footer class="text-center" style="background: #ebd9d5;">
-        <div class="container text-muted py-4 py-lg-5">
-            <ul class="list-inline">
-                <li class="list-inline-item me-4"><a class="link-secondary" href="#" style="font-size: 17px;">Produits</a></li>
-                <li class="list-inline-item me-4"><a class="link-secondary" href="#" style="font-size: 17px;">Connexion</a></li>
-                <li class="list-inline-item"><a class="link-secondary" href="#" style="font-size: 17px;">Contact</a></li>
-            </ul>
-            <p class="mb-0">Copyright © 2022 Raphia</p>
-        </div>
-    </footer>
-</div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <div>
+        <footer class="text-center" style="background: #ebd9d5;">
+            <div class="container text-muted py-4 py-lg-5">
+                <ul class="list-inline">
+                    <li class="list-inline-item me-4"><a class="link-secondary" href="#" style="font-size: 17px;">Produits</a></li>
+                    <li class="list-inline-item me-4"><a class="link-secondary" href="#" style="font-size: 17px;">Connexion</a></li>
+                    <li class="list-inline-item"><a class="link-secondary" href="#" style="font-size: 17px;">Contact</a></li>
+                </ul>
+                <p class="mb-0">Copyright © 2022 Brand</p>
+            </div>
+        </footer>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
