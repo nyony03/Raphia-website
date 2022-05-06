7c8bdf9d1436fb50832a6f913106af4cf110f0f3
Babybadass <obada.tabbab@etu.umontpellier.fr>
2022-05-06 19:21:51 +0200

    gros bug

--- view/panier/panier.php
+++ view/panier/panier.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,37 +10,41 @@
 </head>
 
 <body>
-    <!-- la div entete à ne pas toucher -->
+    <nav class="navbar navbar-light navbar-expand-md" style="background: #ebd9d5;">
+        <div class="container-fluid"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><img src="assets/img/logo.png" style="width: 50px;">
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(167,91,91);font-weight: bold;font-size: 20px;letter-spacing: 1px;font-family: 'Abhaya Libre', serif;">Raphia</a></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                </ul><button class="btn btn-primary d-xxl-flex" type="button" style="background: rgb(211,110,112);margin-left: auto;font-family: 'Abhaya Libre', serif;font-weight: bold;font-size: 18px;letter-spacing: 1px;border-color: rgb(211,110,112);border-top-color: rgb(255,;border-right-color: 255,;border-bottom-color: 255);border-left-color: 255,;">Connexion</button>
+            </div>
+        </div>
+    </nav>
     <div class="d-flex flex-row flex-shrink-1 justify-content-between" style="padding-bottom: 22px;padding-top: 8px;">
         <div class="d-flex flex-row flex-shrink-1">
             <div class="d-flex flex-column flex-shrink-1" style="padding-left: 98px;">
-                <p style="font-size: 22px;font-weight: bold;letter-spacing: 1px;padding-left: 50px;">Produits</p>
+                <p style="font-size: 22px;font-weight: bold;letter-spacing: 1px;padding-left: 50px;color: #a75b5b;">Produits</p>
                 <div class="d-flex flex-row"></div>
             </div>
         </div>
-        <p class="d-flex flex-shrink-1 justify-content-xl-center" style="padding-right: 20px;font-weight: bold;font-size: 20px;letter-spacing: 1px;"><strong>Total</strong></p>
+        <p class="d-flex flex-shrink-1 justify-content-xl-center" style="padding-right: 20px;font-weight: bold;font-size: 20px;letter-spacing: 1px;color: rgb(167,91,91);border-color: rgb(167,91,91);"><strong>Total</strong></p>
     </div>
-    <!--la div du produit dans le panier--->
     <div class="d-flex flex-row flex-shrink-1 justify-content-between" style="padding-bottom: 12px;">
-        <div class="d-flex flex-row flex-shrink-1 align-content-center"><img>
+        <div class="d-flex flex-row flex-shrink-1 align-content-center"><img style="width: 120px;height: 120px;">
             <div class="d-flex flex-column flex-shrink-1" style="padding-left: 50px;">
-                <!-- celle ci est à demander en php -->
-                <p style="font-size: 18px;font-weight: bold;letter-spacing: 1px;">NomProduit&nbsp;</p>
+                <p style="font-size: 18px;font-weight: bold;letter-spacing: 1px;color: rgb(0,0,0);">NomProduit&nbsp;</p>
                 <div class="d-flex flex-row">
-                    <p style="font-size: 18px;letter-spacing: 1px;font-style: italic;">Quantité&nbsp; :</p>
-                    <!-- celle ci est à calculer en php -->
+                    <p style="font-size: 18px;letter-spacing: 1px;font-style: italic;color: rgb(0,0,0);">Quantité&nbsp; :</p>
                     <p style="font-size: 18px;letter-spacing: 1px;padding-left: 15px;">int&nbsp;</p>
                 </div>
-                <div class="d-flex flex-row justify-content-evenly"><button class="btn btn-primary" type="button" style="font-size: 16px;font-weight: bold;background: #d36e70;border-color: #d36e70;">+</button><button class="btn btn-primary" type="button" style="font-weight: bold;background: #d36e70;border-color: #d36e70;">-</button></div>
+                <div class="d-flex flex-row justify-content-evenly"><button class="btn btn-primary" type="button" style="font-size: 16px;font-weight: bold;background: rgb(211,110,112);border-color: rgb(211,110,112);">-</button><button class="btn btn-primary" type="button" style="font-weight: bold;background: rgb(211,110,112);border-color: rgb(211,110,112);">+</button></div>
             </div>
         </div>
-        <!-- à calculer en php -->
-        <p class="d-flex flex-shrink-1 justify-content-xl-center" style="padding-right: 20px;font-size: 18px;letter-spacing: 1px;"><strong>Total</strong></p>
+        <p class="d-flex flex-shrink-1 justify-content-xl-center" style="padding-right: 20px;font-size: 18px;letter-spacing: 1px;color: rgb(0,0,0);"><strong>Total</strong></p>
     </div>
-
-    <!-- la div du montant total -->
     <div class="d-flex flex-column flex-shrink-1 align-items-end" style="padding-bottom: 22px;padding-top: 8px;">
-        <p class="d-flex flex-shrink-1 justify-content-xl-center" style="padding-right: 20px;font-weight: bold;font-size: 20px;letter-spacing: 1px;"><strong>Total</strong></p>
+        <p class="d-flex flex-shrink-1 justify-content-xl-center" style="padding-right: 20px;font-weight: bold;font-size: 20px;letter-spacing: 1px;color: rgb(167,91,91);"><strong>Total</strong></p>
         <p style="font-size: 19px;letter-spacing: 1px;">TotalInt&nbsp;</p>
     </div>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
--- view/panierVide/panierVide.php
+++ view/panierVide/panierVide.php
@@ -1,46 +1,31 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Untitled (Backup 1651764693972)</title>
+    <title>Untitled</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abhaya+Libre&amp;display=swap">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
-<body class="d-flex flex-column flex-shrink-1 justify-content-center align-items-center">
-
-    <nav class="navbar navbar-light navbar-expand-md d-flex justify-content-between align-self-start justify-content-xxl-end align-items-xxl-start" style="background: #ebd9d5;">
-        <div class="container-fluid">
-            <div>
-                <picture><img src="assets/img/logo.png" width="50" height="50"></picture><a class="navbar-brand" href="#" style="color: #a75b5b;"><strong><em>Raphia</em></strong></a>
-
-                <?php
-                //à changer en php en fonction de la connexion du client--->
-                //l'utilisateur n'est pas connecté
-                if(!isset($_SESSION['nom'])) {
-                    //aide pour la gestion des boutons à changer l'action du routeur je pense
-                    echo '</div><button class="btn btn-primary" type="button" style="background: #d36e70;color: rgb(255,255,255);border-width: 0px;opacity: 1;">Connexion</button>'
-                }
-                else{
-                    echo '<p style="color: #ad5355;font-weight: bold;font-size: 18px;letter-spacing: 0.5px;">'.$_SESSION['nom'].'</p>';
-
-                }
-                ?>
-
+<body class="d-flex flex-column">
+    <nav class="navbar navbar-light navbar-expand-md" style="background: #ebd9d5;">
+        <div class="container-fluid"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><img src="assets/img/logo.png" style="width: 50px;">
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(167,91,91);font-weight: bold;font-size: 20px;letter-spacing: 1px;font-family: 'Abhaya Libre', serif;">Raphia</a></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                </ul><button class="btn btn-primary d-xxl-flex" type="button" style="background: rgb(211,110,112);margin-left: auto;font-family: 'Abhaya Libre', serif;font-weight: bold;font-size: 18px;letter-spacing: 1px;">Connexion</button>
+            </div>
         </div>
     </nav>
-    <div>
-        <div class="d-flex d-lg-flex flex-column justify-content-center align-items-center align-content-around" style="padding-top: 110px;"><i class="fas fa-shopping-basket" style="font-size: 170px;color: rgba(211,110,112,0.57);"></i>
-            <p style="font-size: 20px;letter-spacing: 1px;color: rgb(255,255,255);background: #d36e7092;border-radius: 22px;padding-left: 6px;padding-right: 6px;">Votre panier est vide</p>
-            <p style="color: rgb(211,110,112);text-decoration: underline;font-weight: bold;letter-spacing: 1px;">Accueil</p>
-        </div>
+    <div class="d-flex d-lg-flex flex-column justify-content-center align-items-center align-content-around" style="padding-top: 110px;"><i class="fas fa-shopping-basket" style="font-size: 170px;color: rgba(211,110,112,0.57);"></i>
+        <p style="font-size: 20px;letter-spacing: 1px;color: rgb(255,255,255);background: #d36e7092;border-radius: 22px;padding-left: 6px;padding-right: 6px;font-family: 'Abhaya Libre', serif;">Votre panier est vide</p>
+        <p style="color: rgb(211,110,112);text-decoration: underline;font-weight: bold;letter-spacing: 1px;font-family: 'Abhaya Libre', serif;">Accueil</p>
     </div>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
